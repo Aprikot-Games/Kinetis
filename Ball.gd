ffb1385778e161d568124f66de11363319c30317
Gecko05 <jacacent@gmail.com>
2021-07-01 22:48:08 -0500

    Adding directional arrow

--- Ball.gd
+++ Ball.gd
@@ -1,16 +1,26 @@
 extends RigidBody2D
 
-
+enum STATE {BASE, BOUNCE}
 var holding = false
-
+var ball_state = STATE.BASE
+var initial_mouse
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 func _process(delta):
-	
+	if ball_state == STATE.BASE:
+		if holding = true:
+			$Arrow.show()
+		else:
+			$Arrow.hide()
+	pass
 
 func _unhandled_input(event):
 	if event is InputEventMouseButton:
 		if event.button_index == BUTTON_LEFT:
-			holding = event.pressed
+			if event.pressed == true:
+				initial_mouse = get_global_mouse_position()
+				holding = true
+			elif event.pressed == false:
+				holding = false
