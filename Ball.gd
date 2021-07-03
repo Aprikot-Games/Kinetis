00fb88a00ad2e6623874552cf134c8d6e52fc7f2
Gecko05 <jacacent@gmail.com>
2021-07-03 14:03:21 -0500

    Refined ball destruction

--- Ball.gd
+++ Ball.gd
@@ -6,17 +6,31 @@ var ball_state = STATE.RESIZE
 var initial_mouse
 var diff_vec = 0
 var clamped_len = 0
-var ball_power = 1
+var ball_power = 3
+var bounces = 3
+const initial_pos = Vector2(255, 443)
+
+signal ball_destroyed
+
+var ball2_tex = preload("res://sprites/ball2.png")
+var ball3_tex = preload("res://sprites/ball3.png")
+var ball4_tex = preload("res://sprites/ball4.png")
+var ball5_tex = preload("res://sprites/ball5.png")
+var ball6_tex = preload("res://sprites/ball6.png")
+
+var ball_texs = [ball2_tex, ball3_tex, ball4_tex, ball5_tex, ball6_tex]
 
 func release():
 	if ball_state == STATE.LAUNCH:
 		apply_central_impulse(diff_vec.normalized() * clamped_len * 5)
+		ball_state = STATE.BOUNCE
+		$Arrow.hide()
 	if ball_state == STATE.RESIZE:
 		ball_state = STATE.LAUNCH
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	position = initial_pos
 
 func _process(delta):
 	if ball_state == STATE.LAUNCH:
@@ -37,9 +51,9 @@ func _process(delta):
 			$Arrow.offset.y = (vec_len * 18) + 16
 			$Arrow/ArrowBody.position.y = vec_len * 18 + 16
 			mass = 1
-			ball_power = floor(vec_len) + 1
+			ball_power = 5 - (floor(vec_len)+1)
 			print(ball_power)
-	pass
+	$Sprite.set_texture(ball_texs[ball_power])
 
 func _unhandled_input(event):
 	if event is InputEventMouseButton:
@@ -53,5 +67,11 @@ func _unhandled_input(event):
 
 func _on_Ball_body_entered(body):
 	if body.is_in_group("block"):
-		body.damage(ball_power)
+		body.damage(1)
 		pass
+	bounces -= 1
+	if bounces <= 0:
+		ball_power-= 1
+		bounces = 3
+	if ball_power < 0:
+		emit_signal("ball_destroyed")
--- Level.gd
+++ Level.gd
@@ -1,11 +1,18 @@
 extends Node2D
 
 export (PackedScene) var Block
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+export (PackedScene) var Ball
 
+var player_ball
 
-# Called when the node enters the scene tree for the first time.
+func create_new_ball():
+	player_ball = Ball.instance()
+	add_child(player_ball)
+	player_ball.connect("ball_destroyed", self, "_on_ball_destroyed")
+
 func _ready():
-	pass # Replace with function body.
+	create_new_ball()
+
+func _on_ball_destroyed():
+	player_ball.queue_free()
+	create_new_ball()
